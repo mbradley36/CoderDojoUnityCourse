41b9a8e45ea52d295452dcc45aff47fe10b179f1
mbradley36 <mirandajbradley@gmail.com>
2015-08-23 13:48:00 -0700

    fixed forward vector on bullets

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -7,9 +7,10 @@ public class Bullet : MonoBehaviour {
 	void Update () {
 		//The bullet will always move the direction it is facing, which was
 		//	set when we spawned it in the Player class.
-		//We move it forward by getting this forward vector, then adding it
+		//We move it forward by getting this forward vector(we'll consider the 
+		//	character's forward to be to the right of the sprite), then adding it
 		//	to the bullet's current position.
-		Vector2 forward = transform.forward;
+		Vector2 forward = transform.right;
 		Vector2 nextPosition = transform.position;
 		nextPosition.x += forward.x;
 		nextPosition.y += forward.y;
